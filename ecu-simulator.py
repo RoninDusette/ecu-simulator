8f182e85977e581b0cf4974f9dad3818c10273f5
tmkdev <tekolody@gmail.com>
2019-12-20 18:39:39 +0000

    Added .gitignore, modularized sensors and made a sensor class.

--- ecu-simulator.py
+++ ecu-simulator.py
@@ -4,59 +4,50 @@ import logging
 from random import randint
 import can
 from can.bus import BusState
+from sensors import RandHistory, SupportedPids
+
+sensors = {
+    0x04: { 'name': 'Engine Load', 'sensor': RandHistory(bytesize=1, initval=0, randstep=3)},
+    0x05: { 'name': 'Engine Coolant Temp', 'sensor': RandHistory(bytesize=1, initval=128, randstep=1)},
+    0x06: { 'name': 'STFT Bank 1', 'sensor': RandHistory(bytesize=1, initval=128, randstep=1)},
+    0x07: { 'name': 'LTFT Bank 1', 'sensor': RandHistory(bytesize=1, initval=128, randstep=1)},
+    0x08: { 'name': 'STFT Bank 2', 'sensor': RandHistory(bytesize=1, initval=128, randstep=1)},
+    0x09: { 'name': 'LTFT Bank 2', 'sensor': RandHistory(bytesize=1, initval=128, randstep=1)},
+    0x0A: { 'name': 'Fuel Pressure', 'sensor': RandHistory(bytesize=1, initval=128, randstep=1)},
+    0x0B: { 'name': 'Manifold Absolute Pressure', 'sensor': RandHistory(bytesize=1, initval=128, randstep=1)},
+    0x0C: { 'name': 'RPM', 'sensor': RandHistory(bytesize=2, initval=2400, randstep=200)},
+    0x0D: { 'name': 'Speed', 'sensor': RandHistory(bytesize=1, initval=0, randstep=3)},
+    0x0E: { 'name': 'Timing Advance', 'sensor': RandHistory(bytesize=1, initval=128, randstep=1)},
+    0x0F: { 'name': 'Intake Air Temp', 'sensor': RandHistory(bytesize=1, initval=128, randstep=1)},
+    0x10: { 'name': 'MAF rate', 'sensor': RandHistory(bytesize=2, initval=400, randstep=100)},
+    0x11: { 'name': 'Throttle Position', 'sensor': RandHistory(bytesize=1, initval=128, randstep=1)},
+}
+
+supportedpid = SupportedPids(sensors)
 
 def service1(bus, msg):
-    if msg.data[2] == 0x00:
-        logging.info(">> Caps")
+    arbid = msg.data[2]
+    if arbid == 0x00:
+        logging.info(">> Supported Pids")
         msg = can.Message(arbitration_id=0x7e8,
-          data=[0x06, 0x41, 0x00, 0xBF, 0xDF, 0xB9, 0x91],
+          data=[0x06, 0x41, arbid] + supportedpid.next(),
           is_extended_id=False)
         bus.send(msg)
-    elif msg.data[2] in [0x20, 0x40, 0x60, 0x80, 0xA0, 0xC0]:
-        print(">> Extented Pid Support")
+    elif arbid in [0x20, 0x40, 0x60, 0x80, 0xA0, 0xC0]:
+        logging.info(">> Extented Pid Support")
         msg = can.Message(arbitration_id=0x7e8,
-          data=[0x06, 0x41, msg.data[2], 0x00, 0x00, 0x00, 0x00],
+          data=[0x06, 0x41, arbid, 0x00, 0x00, 0x00, 0x00],
           is_extended_id=False)
         bus.send(msg)
-    elif msg.data[2] == 0x0C:
-        print(">> RPM")
+    elif arbid in sensors:
+        sensor = sensors[arbid]
+        logging.info(f">> {sensor['name']}")
         msg = can.Message(arbitration_id=0x7e8,
-          data=[0x04, 0x41, 0x0C, 0x12, 0x13],
-          is_extended_id=False)
-        bus.send(msg)
-    elif msg.data[2] == 0x0D:
-        print(">> Speed")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x03, 0x41, 0x0D, randint(40, 60)],
-          is_extended_id=False)
-        bus.send(msg)
-    elif msg.data[2] == 0x10:
-        print(">> MAF air flow rate")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x04, 0x41, 0x10, 0x00, 0xFA],
-          is_extended_id=False)
-        bus.send(msg)
-    elif msg.data[2] == 0x04:
-        print(">> Calculated engine load")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x03, 0x41, 0x04, 0x20],
-          is_extended_id=False)
-        bus.send(msg)
-    elif msg.data[2] == 0x05:
-        print(">> Engine coolant temperature")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x03, 0x41, 0x05, 0x82],
-          is_extended_id=False)
-        bus.send(msg)
-    elif msg.data[2] == 15:
-        print(">> Intake air temperature")
-        msg = can.Message(arbitration_id=0x7e8,
-          data=[0x03, 0x41, 0x0F, 0x56],
+          data=[ 2 + sensor['sensor'].get_bytesize(), 0x41, arbid] + sensor['sensor'].next(),
           is_extended_id=False)
         bus.send(msg)
     else:
-        print("!!! Service 1, unknown code", msg.data[2])
-
+        logging.warning(f"!!! Service 1, unimplemented arbid->{arbid}")
 
 def receive_all():
     bus = can.interface.Bus(bustype='socketcan',channel='can0')
